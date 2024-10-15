15545562eee6fc4585e892c31c66a592c640bc69
newraina <newraina@users.noreply.github.com>
2024-10-15 20:35:48 +0700

    remove desc field of envelope

--- src/components/activity-manage/coin-envelope-detail-table.tsx
+++ src/components/activity-manage/coin-envelope-detail-table.tsx
@@ -8,13 +8,6 @@ export default function CoinEnvelopeDetailTable({ data }: CoinEnvelopeDetailTabl
   return (
     <dl className="grid grid-cols-1 text-base/6 sm:grid-cols-[min(50%,theme(spacing.80))_auto] sm:text-sm/6">
       <dt className="col-start-1 border-t border-gray-950/5 pt-3 text-gray-500 first:border-none dark:border-white/5 dark:text-gray-400 sm:py-3 sm:dark:border-white/5">
-        Description
-      </dt>
-      <dd className="sm:[&amp;:nth-child(2)]:border-none pb-3 pt-1 text-gray-950 dark:text-white sm:py-3 dark:sm:border-white/5">
-        {data.description}
-      </dd>
-
-      <dt className="col-start-1 border-t border-gray-950/5 pt-3 text-gray-500 first:border-none dark:border-white/5 dark:text-gray-400 sm:py-3 sm:dark:border-white/5">
         Created at
       </dt>
       <dd className="sm:[&amp;:nth-child(2)]:border-none pb-3 pt-1 text-gray-950 dark:text-white sm:border-t sm:border-gray-950/5 sm:py-3 dark:sm:border-white/5">
--- src/components/activity-manage/nft-envelope-detail-table.tsx
+++ src/components/activity-manage/nft-envelope-detail-table.tsx
@@ -15,13 +15,6 @@ export default function NFTEnvelopeDetailTable({ data }: NFTEnvelopeDetailTableP
         {formatCoinType(data.nftType)}
       </dd>
       <dt className="col-start-1 border-t border-gray-950/5 pt-3 text-gray-500 first:border-none dark:border-white/5 dark:text-gray-400 sm:py-3 sm:dark:border-white/5">
-        Description
-      </dt>
-      <dd className="sm:[&amp;:nth-child(2)]:border-none pb-3 pt-1 text-gray-950 dark:text-white sm:py-3 dark:sm:border-white/5">
-        {data.description}
-      </dd>
-
-      <dt className="col-start-1 border-t border-gray-950/5 pt-3 text-gray-500 first:border-none dark:border-white/5 dark:text-gray-400 sm:py-3 sm:dark:border-white/5">
         Created at
       </dt>
       <dd className="sm:[&amp;:nth-child(2)]:border-none pb-3 pt-1 text-gray-950 dark:text-white sm:border-t sm:border-gray-950/5 sm:py-3 dark:sm:border-white/5">
