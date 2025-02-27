c5d19823836123d8b9c93270b3cfa750c3c19547
newraina <newraina@users.noreply.github.com>
2025-02-27 18:02:52 +0800

    feat(ui): enhance user profile dropdown functionality

--- src/components/top-right-actions.tsx
+++ src/components/top-right-actions.tsx
@@ -1,21 +1,20 @@
 'use client';
 
-import { useAppSession, useWalletHexAddress } from '@/hooks';
+import { useAppSession, useToast, useWalletHexAddress } from '@/hooks';
 import { NETWORK_NAME } from '@/utils/constants';
-import {
-  useCurrentWallet,
-  useWalletStore
-} from '@roochnetwork/rooch-sdk-kit';
-import { useState } from 'react';
-import { HashAvatar } from './hash-avatar';
+import { useCurrentWallet, useWalletStore } from '@roochnetwork/rooch-sdk-kit';
+import { useMemo, useState } from 'react';
 import { Button } from './ui/button';
 
+import { Link } from '@/i18n/routing';
+import { CopyIcon, UserRoundIcon } from 'lucide-react';
 import { useTranslations } from 'next-intl';
 import { LanguageSwitcher } from './language-switcher';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu';
 import { WalletConnectDialog } from './wallet-connect-dialog';
@@ -24,37 +23,60 @@ export function TopRightActions() {
   const { sessionKey, handlerCreateSessionKey, sessionLoading } = useAppSession();
   const currentWallet = useCurrentWallet();
   const hexAddress = useWalletHexAddress();
+  const shortHexAddress = useMemo(
+    () => `${hexAddress.slice(0, 6)}...${hexAddress.slice(-4)}`,
+    [hexAddress]
+  );
   const setWalletDisconnected = useWalletStore((state) => state.setWalletDisconnected);
 
   const [connectModalOpen, setConnectModalOpen] = useState(false);
 
   const t = useTranslations();
+  const { toast } = useToast();
 
   return (
     <div className="flex items-center gap-4 text-sm">
       <LanguageSwitcher />
-      <div className="text-foreground/60">
-        {NETWORK_NAME}
-      </div>
+      <div className="text-foreground/60">{NETWORK_NAME}</div>
       <div>
         {currentWallet.isConnected ? (
           <div className="flex items-center gap-2">
             {sessionKey ? (
               <DropdownMenu>
                 <DropdownMenuTrigger className="rounded-full">
-                  <HashAvatar
-                    className="h-5 w-5 cursor-pointer rounded-full bg-white transition-all hover:shadow-sm"
-                    address={hexAddress}
-                  />
+                  <div className="flex h-5 w-5 items-center justify-center rounded-full text-foreground/60 transition-all hover:text-foreground/80 hover:shadow-sm">
+                    <UserRoundIcon strokeWidth={2} className="h-4 w-4" />
+                  </div>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
+                  <DropdownMenuItem
+                    className="font-mono text-xs"
+                    onClick={() => {
+                      navigator.clipboard.writeText(hexAddress);
+                      toast({
+                        title: t('common.copied'),
+                      });
+                    }}
+                  >
+                    <div>{shortHexAddress}</div>
+                    <CopyIcon className="ml-1 h-3 w-3" />
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <Link href="/activities">{t('navigation.myActivities')}</Link>
+                  </DropdownMenuItem>
                   <DropdownMenuItem onClick={() => setWalletDisconnected()}>
                     {t('common.disconnect')}
                   </DropdownMenuItem>
                 </DropdownMenuContent>
               </DropdownMenu>
             ) : (
-              <Button size="sm" variant="outline" disabled={sessionLoading} onClick={handlerCreateSessionKey}>
+              <Button
+                size="sm"
+                variant="outline"
+                disabled={sessionLoading}
+                onClick={handlerCreateSessionKey}
+              >
                 {t('session.create')}
               </Button>
             )}
@@ -69,10 +91,7 @@ export function TopRightActions() {
           </Button>
         )}
       </div>
-      <WalletConnectDialog
-        open={connectModalOpen}
-        onOpenChange={setConnectModalOpen}
-      />
+      <WalletConnectDialog open={connectModalOpen} onOpenChange={setConnectModalOpen} />
     </div>
   );
 }
